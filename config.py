e28bbdfb0742ebe790d5863dc28b7da62fa372c5
sfskysf <67379914+sfskysf@users.noreply.github.com>
2021-01-05 23:44:00 +0200

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode. for the debug toolbar
 DEBUG = True
+heroku_uri = 'postgres://gxtdvxhtbgetfi:969542fa57489f1c593c478a56f9cd8dff95965787c4e80745f55181e61d0797@ec2-3-231-241-17.compute-1.amazonaws.com:5432/dd10eco1qeka5g'
+local_uri = 'postgresql://postgres:root@localhost:5432/attendance_test'
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost:5432/attendance_test'
+SQLALCHEMY_DATABASE_URI = heroku_uri
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
